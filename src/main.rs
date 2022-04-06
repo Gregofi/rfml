bfc7824b084e95e5bd470c0b416c4560e7815fd7
Filip Gregor <filip.gregor98@gmail.com>
2022-04-06 21:15:54 +0200

    Rework errors

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,15 @@ use std::fs;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let program = fs::read_to_string(&args[2]);
-    let tree: AST = serde_json::from_str(&program.unwrap()).unwrap();
-    compile(&tree)
+    if args.len() < 3 {
+        panic!("Usage: fml command file");
+    }
+
+    if args[1] == "compile" {
+        let program = fs::read_to_string(&args[2]).unwrap();
+        let tree: AST = serde_json::from_str(&program).unwrap();
+        compile(&tree)
+    } else {
+        panic!("Following commands are supported: compile")
+    }
 }
